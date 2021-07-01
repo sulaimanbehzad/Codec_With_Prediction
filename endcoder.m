cf2b121f1c5d4dc5d89f6371776912d816dd6b65
sulaimanbehzad <sulaimanbehzad@gmail.com>
2021-07-01 15:44:46 +0430

    estimataion added to encoder

--- endcoder.m
+++ endcoder.m
@@ -14,10 +14,19 @@ for i=1:nImages
 % Dimensions for DCT blocksize
     A=8;
     B=8;
+    N=8;
     [imX, imY] = size(curImage);
-    curDct=ComputeDCT(curImage, A, B, imX, imY);
-    curZigZag = ComputeZigZag(curDct, A, B, imX, imY, 8, 8);
-    curRunLength = RunLength(curZigZag, A, B, imX, imY);
-%     saves runlength in .mat format
-    save(['runlength/' imageFiles(i).name '.mat'], 'curRunLength');
+    if i == 1 || (mod(i,5) == 0)
+        iFrame = IntraPrediction(curImage,imX,imY,N);
+        curDct=ComputeDCT(iFrame, A, B, imX, imY);
+        curZigZag = ComputeZigZag(curDct, A, B, imX, imY, 8, 8);
+        curRunLength = RunLength(curZigZag, A, B, imX, imY);
+        save(['runlength/iFrames' imageFiles(i).name '.mat'], 'curRunLength');
+    else
+        [pFrame,pred_err]=MotionEstimation(iFrame,curImage,imX,imY,N);
+        curDct=ComputeDCT(pFrame, A, B, imX, imY);
+        curZigZag = ComputeZigZag(curDct, A, B, imX, imY, 8, 8);
+        curRunLength = RunLength(curZigZag, A, B, imX, imY);
+        save(['runlength/pFrames' imageFiles(i).name '.mat'], 'curRunLength');
+    end
 end
